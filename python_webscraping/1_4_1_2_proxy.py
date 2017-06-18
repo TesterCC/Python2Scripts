16e6be4a0cc5e570e7749bf260e62b8ed611e537
LYX <testerlyx@foxmail.com>
2017-06-18 23:57:10 +0800

    20170618_01, add py2.7 set proxy demo.

--- python_webscraping/1_4_1_2_proxy.py
+++ python_webscraping/1_4_1_2_proxy.py
@@ -2,4 +2,24 @@
 # coding:utf-8
 # 用Python写网络爬虫 P11-12
 # http://www.meetup.com/     # don't allow User-Agent:Python-urllib/2.7
+import urllib2
 
+
+def download(url, user_agent='wswp', num_retries=2):
+    print 'Downloading: ', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
+    try:
+        html = urllib2.urlopen(request).read()
+    except urllib2.URLError as e:
+        print 'Downloading error: ', e.reason
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # retry 5XX HTTP errors
+                return download(url, user_agent, num_retries-1)
+    return html
+
+
+if __name__ == '__main__':
+    print download("http://www.meetup.com")
