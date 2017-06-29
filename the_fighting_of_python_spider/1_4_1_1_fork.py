65ffd49fd00779f3b443f57b9e1735c9f1ac0bd9
LYX <testerlyx@foxmail.com>
2017-06-29 23:23:07 +0800

    20170629_01, add fork and multiprocessing demo.

---
+++ the_fighting_of_python_spider/1_4_1_1_fork.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+# 使用fork方式实现多进程
+
+import os
+
+
+def testfork():
+    print "Current Process (%s) start ..." % (os.getpid())
+    pid = os.fork()
+    if pid < 0:
+        print "Error in fork."
+    elif pid == 0:
+        print 'I am child process (%s) and my parent process is (%s)' % (os.getpid(), os.getppid())
+    else:
+        print '(%s) created a child process (%s).' % (os.getpid(), pid)
+
+if __name__ == '__main__':
+    testfork()
