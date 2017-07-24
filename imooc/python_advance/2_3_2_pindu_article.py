0d6c609154f42fd0627b8fccf1dcd8a5fa136756
LYX <testerlyx@foxmail.com>
2017-07-24 23:19:57 +0800

    20170724_01, add 2-3 demo, finish dict statistics demo1.

---
+++ imooc/python_advance/2_3_2_pindu_article.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+import re
+
+# http://coding.imooc.com/lesson/62.html#mid=837
+# 2. 对某英文文章的单词，进行词频统计，找到出现次数最高的10个单词，它们出现次数是多少？
+# 统计文章：Linux kernel coding style， Linux kernel coding style.txt
+# 地址：https://www.kernel.org/doc/html/latest/process/coding-style.html
