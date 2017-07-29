102d7cc57ea708b4bb449e52c3bc0ddc7d2fb4ed
LYX <testerlyx@foxmail.com>
2017-07-29 10:40:58 +0800

    20170729_02, add study.py as ConfigParse demo.

--- imooc/io_file/4_1_practice/study.py
+++ imooc/io_file/4_1_practice/study.py
@@ -20,5 +20,49 @@ import ConfigParser
 '''
 
 
+class student_info(object):
+
+    def __init__(self, recordfile):
+        self.logfile = recordfile
+        self.cfg = ConfigParser.ConfigParser()
+
+    def cfg_load(self):
+        self.cfg.read(self.logfile)
+
+# config file load in memory, so will not change *.ini file
+    def cfg_dump(self):
+        se_list = self.cfg.sections()
+        print("----------------------")
+        for se in se_list:
+            print se
+            print self.cfg.items(se)
+        print("----------------------")
+
+    def delete_item(self, section, key):
+        self.cfg.remove_option(section, key)
+
+    def delete_section(self, section):
+        self.cfg.remove_section(section)
+
+    def add_section(self, section):
+        self.cfg.add_section(section)
+
+    def set_item(self, section, key, value):
+        self.cfg.set(section, key, value)
+
+    def save(self):
+        fp = open(self.logfile, 'w')    # Here self.logfile == 'imooc.txt'
+        self.cfg.write(fp)
+        fp.close()
 
 
+if __name__ == '__main__':
+    info = student_info('imooc.txt')
+    info.cfg_load()
+    info.cfg_dump()
+    info.set_item('userinfo', 'pwd', 'abc')
+    info.cfg_dump()
+    info.add_section('login')
+    info.set_item('login', '20150511', '20')
+    info.cfg_dump()
+    info.save()
