933e234688be0b53436f4176421cda4ab797e9d3
LYX <testerlyx@foxmail.com>
2017-08-26 23:09:30 +0800

    20170826_, code for makePasswordFileFunction.py

--- python_web_scraping/makePasswordFileFunction.py
+++ python_web_scraping/makePasswordFileFunction.py
@@ -57,8 +57,44 @@ def main():
 
 def mainMenu():
     '''主菜单'''
+    global DenyList
+    global rawList
+    global pwList
+    global flag
+    clear()
+    print(u"||"),
+    print(u"="*40),
+    print(u"||")
+    print(u"|| 0: 退出程序")
+    print(u"|| 1: 输入密码原始字符串")
+    print(u"|| 2: 添加非法字符到列表")
+    print(u"|| 3: 清空原始密码列表")
+    print(u"|| 4: 整理原始密码列表")
+    print(u"|| 5: 改变默认密码长度(%d-%d)" % (minLen, maxLen))
+    print(u"|| 6: 创建密码列表")
+    print(u"|| 7: 显示所有密码")
+    print(u"|| 8: 创建字典文件")
+    print(u"||")
+    print(u"||"),
+    print(u"=" * 40),
+    print(u"||")
+    print(u"当前非法字符为: %s" % denyList)
+    print(u"当前原始密码元素为: %s" % rawList)
+    print(u"共有密码%d个" % len(pwList))
 
-    pass
+    if flag:
+        print(u"已在当前目录创建密码文件dic.txt")
+    else:
+        print(u"尚未创建密码文件")
+
+
+def clear():
+    '''清屏函数'''
+    os = platform.system()
+    if (os == u"Windows"):
+        os.system('cls')
+    else:
+        os.system('clear')
 
 
 def tipMainMenuInputError():
