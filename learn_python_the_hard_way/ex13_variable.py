f041dbc429f8047fef5a310321436713dfef6909
LYX <testerlyx@foxmail.com>
2017-09-15 22:55:53 +0800

    20170915_01, add learn python the hard way demo.

---
+++ learn_python_the_hard_way/ex13_variable.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = 'TesterCC'
+# __time__   = '17/9/15 22:47'
+
+
+'''
+笨办法学python P44
+'''
+
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+
+# useage
+# in terminal
+# python ex13_variable.py 1st 2ed 3rd
